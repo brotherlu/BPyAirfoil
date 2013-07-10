b3262072dac8a5b6f7caa83a335fcf598fd27c96
Louay Cheikh <brotherlu@gmail.com>
2013-07-10 11:52:03 -0400

    Update bpyFoil.py
    # Implemented import as Tri-Surface
    # Removed Panel (was unneeded)

--- bpyFoil.py
+++ bpyFoil.py
@@ -34,7 +34,7 @@ bl_info = 	{
 			'name':'Blender Airfoil Importer',
 			'category':'Object',
 			'author':'Louay Cheikh',
-			'version':(0,3),
+			'version':(0,4),
 			'blender':(2,65,0),
 			'location':'Search Menu'
 			}
@@ -45,12 +45,17 @@ def SelectObj(ObjName):
 	Obj.select = True
 	bpy.context.scene.objects.active = Obj
 
+def SelectNewestObj():
+	obj = bpy.context.scene.objects[0]
+	obj.select = True
+	bpy.context.scene.objects.active = obj
+
 def MakePolyLine(objname, curvename, cList):
 	"""
 	Polyline Generator function generator from 
 	http://blenderscripting.blogspot.ca/2011/05/blender-25-python-bezier-from-list-of.html
 	"""
-	
+
 	w=1 # weight
 	curvedata = bpy.data.curves.new(name=curvename, type='CURVE')  
 	curvedata.dimensions = '3D'  
@@ -70,65 +75,53 @@ class bpyAirfoil(Operator):
 	bl_idname = "object.bpyairfoil"
 	bl_label = "Airfoil DAT File Importer"
 	bl_options = {'REGISTER','UNDO'}
-	
+
 	FileName = StringProperty(name="Filename", subtype="FILE_PATH", default="/tmp\\", description="Dat file location")
 	Mesh_choice = BoolProperty(name="Import as Mesh")
+	Surface_choice = BoolProperty(name="Import as Surface")
 
 	def execute(self, context):
 		""" Import """
-		
 		try:
 			f = open(self.FileName,'r')
 		except:
 			return {'FINISHED'}
-
 		# Read the raw file
 		data = f.readlines()
-
 		# Extract the Airfoil Name
 		FoilName = data[0]
-
 		# Create the regexp for finding the data
 		r = re.compile('[.\S]*\.[0-9]*')
-
 		# Create the coordintes from the regler exp
 		FoilCoords = [r.findall(x) for x in data[1:]]
-
 		# Convert the strings to Floats
 		FoilPoints = [(float(x[0]),0,float(x[1])) for x in FoilCoords if len(x)==2 ]
-
 		# Ensure the First point is not the Point Count that some DAT files include 
 		if FoilPoints[0][0]>1: FoilPoints.remove(FoilPoints[0])
-		
 		# Generate the Polyline
 		MakePolyLine(FoilName,FoilName,FoilPoints)
-		
 		# Select Object
-		SelectObj(FoilName)
-		
-		if self.Mesh_choice:
-			bpy.ops.object.convert(target='MESH',keep_original=False)
-		
-		return {'FINISHED'}
+		SelectNewestObj()
 
-class bpyAirfoil_Panel(Panel):
-	""" Addon Panel """
-	bl_label = "Import Panel"
-	bl_space_type = "VIEW_3D"
-	bl_region_type = "TOOL_PROPS"
-	
-	def draw(self,context):
-		self.layout.operator("object.bpyairfoil",text="Import")
+		if self.Mesh_choice or self.Surface_choice:
+			bpy.ops.object.convert(target='MESH',keep_original=False)
+
+		if self.Surface_choice:
+			bpy.ops.object.editmode_toggle()
+			bpy.ops.mesh.select_all(action="SELECT")
+			bpy.ops.mesh.remove_doubles(threshold=0.0001, use_unselected=False)
+			bpy.ops.mesh.fill(use_beauty=True)
+			bpy.ops.object.editmode_toggle()
+
+		return {'FINISHED'}
 
 def register():
 	""" Register all Classes """
 	bpy.utils.register_class(bpyAirfoil)
-	bpy.utils.register_class(bpyAirfoil_Panel)
 
 def unregister():
 	""" Unregister all Classes """
 	bpy.utils.unregister_class(bpyAirfoil)
-	bpy.utils.unregister_class(bpyAirfoil_Panel)
 
 if __name__ == "__main__":
 	register()
